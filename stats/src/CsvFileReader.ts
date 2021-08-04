95a49bdf79323e5121372f85d5c16ad7f1669f57
Ronald <ronystivens@hotmail.com>
2021-08-04 23:32:32 +0200

    parsed string[] to [date, strings, numbers, enum ...]

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
 
 export class CsvFileReader {
   data: string[][] = [];
@@ -14,6 +15,19 @@ export class CsvFileReader {
       .split('\n')
       .map((row: string): string[] => {
       return row.split(',')
-      });
+      })
+      .map((row: string[]): any => {
+        //[ strings] --> [ parsed items]
+        //has to return an array with 7 items: [date, string, string, number, number, enum string, string ] 
+        return [
+          dateStringToDate(row[0]),
+          row[1],
+          row[2],
+          parseInt(row[3]),
+          parseInt(row[4]),
+          row[5] as MatchResult,
+          row[6]
+        ];
+      })
   }
 }
