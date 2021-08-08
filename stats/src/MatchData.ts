69ef741129838b0a652639b4c945754c8283305e
Ronald <ronystivens@hotmail.com>
2021-08-08 23:49:50 +0200

    new refactor started, implementing composition patter (delegation) so it can be more resusable

---
+++ stats/src/MatchData.ts
@@ -0,0 +1,12 @@
+import { MatchResult } from './MatchResult';
+
+//Defining the tuple
+export type MatchData = [
+    Date, 
+    string, 
+    string, 
+    number, 
+    number, 
+    MatchResult, 
+    string
+];
