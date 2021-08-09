5774ad958d1a49445900794c146137a0083a9d03
Ronald <ronystivens@hotmail.com>
2021-08-10 00:25:34 +0200

    finished refactored

--- stats/src/Summary.ts
+++ stats/src/Summary.ts
@@ -1,14 +1,19 @@
 import { MatchData } from './MatchData';
 
 export interface Analyzer {
-    run(matches: MatchData[]): string;
+  run(matches: MatchData[]): string;
 };
 
 export interface OutputTarget {
-    print(report: string): void;
+  print(report: string): void;
 };
 
 export class Summary {
-    constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {};
+  constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) {};
+
+  buildAndPrintReport(matches: MatchData[]):void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 };
 
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,12 +1,10 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
 import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
 
-//Former approach:
-// const reader = new MatchReader('football.csv');
-// reader.read();
 
-//New approach:
 //Step1. create an object that satisfies the 'DataReader' interface
 const csvFilerReader = new CsvFileReader('football.csv');
 
@@ -14,15 +12,6 @@ const csvFilerReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFilerReader);
 matchReader.load();
 
+const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
 
-let manUnitedWins = 0;
-
-for (let match of matchReader.matches) {
-  if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
-    manUnitedWins++;
-  }
-}
-
-console.log(`Man United won ${manUnitedWins} games`);
+summary.buildAndPrintReport(matchReader.matches);
