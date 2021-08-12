cbe9d76bf1fd718b8639ad57bf7c72d195248307
Ronald <ronystivens@hotmail.com>
2021-08-13 00:33:13 +0200

    last refactored son all new instances are call from a static class

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,18 +1,9 @@
-import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
-import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
+import { MatchReader } from './MatchReader';
+import { Summary } from './Summary';
 
-
-//Step1. create an object that satisfies the 'DataReader' interface
-const csvFilerReader = new CsvFileReader('football.csv');
-
-//Step2. create an instance of MatchReader and pass in something satisfaying the 'DataReader' interface
-const matchReader = new MatchReader(csvFilerReader);
+const matchReader = MatchReader.fromCsv('football.csv');
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
-
+//const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 summary.buildAndPrintReport(matchReader.matches);
