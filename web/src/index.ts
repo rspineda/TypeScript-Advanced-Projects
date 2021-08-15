b02e90b08952f3381344fdb96a3f773e79c12a7f
Ronald <ronystivens@hotmail.com>
2021-08-15 16:37:06 +0200

    handled get and upadte user's data on User's class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,9 @@
-console.log('Starting project!!');
+import { User } from './models/User';
+
+const user = new User({name: 'Ronald', age: 20});
+
+//updating UserInfo
+user.set({name: 'Ronnie'});
+
+console.log(user.get('name'));
+console.log(user.get('age'));
