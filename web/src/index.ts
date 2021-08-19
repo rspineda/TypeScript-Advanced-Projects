da3e369abc6637cece923aa88e9a0abdb4397c00
Ronald <ronystivens@hotmail.com>
2021-08-20 00:26:22 +0200

    fetching user's info  implemented

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,30 +1,17 @@
 import { User } from './models/User';
 import axios from 'axios';
 
-/* const user = new User({name: 'Ronald', age: 20});
-
-//updating UserInfo
-user.set({name: 'Ronnie'});
-
-//testing events registration
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('click', () => {
-  console.log('Click event was triggered');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-
-//console.log(user);
-user.trigger('click');
- */
 
 //testing if backend/databse is working fine:
-const user = {
+/* const user = {
   name: 'Ronald',
   age: 25
 }
 
-axios.post('http://localhost:3000/users', user);
+axios.post('http://localhost:3000/users', user); */
+
+//testing if fetching is working fine:
+const user = new User({ id: 1});
+user.fetch();
+
+setTimeout(() => console.log(user), 4000);
