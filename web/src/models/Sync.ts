1e103ba113bd2beb3186aa319b7565c7093bab2e
Ronald <ronystivens@hotmail.com>
2021-08-25 00:13:13 +0200

    User's attributes refactored so it's encapsulated in a different class

---
+++ web/src/models/Sync.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosPromise } from 'axios';
+import { UserProps } from './User';
+
+interface HasId {
+  id?: number;
+}
+
+export class Sync<T extends HasId> {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`)
+  }
+
+  save(data: T): AxiosPromise {
+    const { id } = data;
+    if(id) {
+      // user is already on database
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      // user is not on database yet
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
+
+/* fetch---->
+.then((response: AxiosResponse): void => {
+      this.set(response.data);
+    });
+*/
