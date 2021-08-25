b1cc3f7983e4ed9da9ced6a6b740838ae43bbff7
Ronald <ronystivens@hotmail.com>
2021-08-26 00:22:23 +0200

    attributes refactored with generics so get data can return the same value as argument received on call

--- web/src/models/Attributes.ts
+++ web/src/models/Attributes.ts
@@ -1,8 +1,10 @@
 export class Attributes<T> {
   constructor(private data: T) {}
   
-  get(propName: string): ( number | string ) {
-    return this.data[propName];
+  //returning the same type as as the 'key' argument, so it will return
+  //number if the key argument is number, string if the key argument is string ..
+  get<K extends keyof T>(key: string): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
