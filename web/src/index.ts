26f8bd7452e730ee6fc1e4346fd78390a9248ad0
Ronald <ronystivens@hotmail.com>
2021-08-31 22:32:35 +0200

    tested fecth method and working as expected

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,12 +1,17 @@
 import { User } from './models/User';
 
 //testing
-const user = new User({ name: 'Manolete', age: 28 });
+/* const user = new User({ name: 'Manolete', age: 28 });
 
 const name = user.get('name');
 console.log(`user's name is: ${name}`);
 
 user.on('change', () => console.log('user was changed !!'));
 //user.trigger('change');
-user.set({ name: 'Manolitus' });
+user.set({ name: 'Manolitus' }); */
+
+//testing feching:
+const user = new User({id: 1});
+user.on('change', () => console.log(user));
+user.fetch();
 
