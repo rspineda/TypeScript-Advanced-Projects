04468656b8d3fc9c39f0e64caeb52bde93cce271
Ronald <ronystivens@hotmail.com>
2021-09-04 00:09:33 +0200

    added collection class so it can be used to fetch/save data from all users...

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,19 @@
 import { User } from './models/User';
+import { Collection } from './models/Collection';
 
 //testing new way of creating users:
-const user = User.buildUser({id:1});
+/* const user = User.buildUser({id:1});
 user.on('change', () => console.log(user));
-user.fetch();
+user.fetch(); */
+
+//testing collection (fetch all users):
+const collection = new Collection('http://localhost:3000/users');
+
+collection.on('change', () => {
+  console.log(collection);
+});
+
+collection.fetch();
+
+
 
