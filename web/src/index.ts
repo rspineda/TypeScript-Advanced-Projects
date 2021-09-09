4b91b963e5f2b94cbc61c046443d3c95472d3fab
Ronald <ronystivens@hotmail.com>
2021-09-09 22:28:55 +0200

    rendered user's info on frontend after user was also passed in the UserForm constructor

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,10 @@
-import { UserForm } from "./views/UserForm";
+import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
+const user = User.buildUser({name: 'Mohama Ali', age: 25})
 const parent = document.getElementById('root');
-const userForm = new UserForm(parent);
+
+const userForm = new UserForm(parent, user);
 
 userForm.render();
 
