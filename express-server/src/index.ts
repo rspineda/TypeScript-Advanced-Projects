7f1bea75041da8d353566943170c6a54f59ba27d
Ronald <ronystivens@hotmail.com>
2021-10-31 23:53:56 +0100

    set routes folder

--- express-server/src/index.ts
+++ express-server/src/index.ts
@@ -1,10 +1,9 @@
 import express, {Request, Response} from 'express';
+import {router} from './routes/loginRoutes';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hi there!!');
-});
+app.use(router);
 
 app.listen(3000, () => {
   console.log('listening on port 3000');
