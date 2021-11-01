6c4f52e3137ad5a040275d737481d3382a9cde8b
Ronald <ronystivens@hotmail.com>
2021-11-02 00:27:33 +0100

    set cookie session handler

--- express-server/src/routes/loginRoutes.ts
+++ express-server/src/routes/loginRoutes.ts
@@ -28,10 +28,29 @@ router.post('/login', (req: RequestWithBody, res: Response) => {
   //harcoded credencials:
   if (email && password && email === 'ronnie@mail.com' && password === 'password') {
     //mark this person as logged in
-
+    req.session = {loggedIn: true};
     //redirect them to the root route
+    res.redirect('/');
   } else {
     res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if(req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
   }
 })
 
