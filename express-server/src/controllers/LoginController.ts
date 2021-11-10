b2a1f07c49beee2287d1a04c65f9f4aa75a38762
Ronald <ronystivens@hotmail.com>
2021-11-10 23:16:10 +0100

    attached decorators to body validation

--- express-server/src/controllers/LoginController.ts
+++ express-server/src/controllers/LoginController.ts
@@ -1,5 +1,5 @@
-import {Request, Response} from 'express';
-import {get, controller} from './decorators';
+import { Request, Response } from 'express';
+import { get, controller, bodyValidator, post} from './decorators';
 
 @controller('/auth')
 class LoginController {
@@ -19,5 +19,21 @@ class LoginController {
         <button>Submit</button>
       </form>
     `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const {email, password} = req.body;
+  
+    //harcoded credencials:
+    if (email && password && email === 'ronnie@mail.com' && password === 'password') {
+      //mark this person as logged in
+      req.session = {loggedIn: true};
+      //redirect them to the root route
+      res.redirect('/');
+    } else {
+      res.send('Invalid email or password');
+    }
   }
 }
